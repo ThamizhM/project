e27d27149c9a32ac1b5a03d314fde74efe678474
thamizhM <thamizhthamizh207@gmail.com>
2023-10-15 11:27:36 +0530

    modified

---
+++ sales.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+from tensorflow.keras.models import sequential
